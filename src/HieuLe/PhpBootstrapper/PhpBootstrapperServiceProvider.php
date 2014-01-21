0bf2819c8b0913a9c1a1fb9f5b55e34851dd1ccc
trunghieule <letrunghieu.cse09@gmail.com>
2014-01-21 15:36:48 +0700

    Create package

---
+++ src/HieuLe/PhpBootstrapper/PhpBootstrapperServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace HieuLe\PhpBootstrapper;
+
+use Illuminate\Support\ServiceProvider;
+
+class PhpBootstrapperServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('hieu-le/php-bootstrapper');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
